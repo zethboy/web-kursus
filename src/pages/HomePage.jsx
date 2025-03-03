12fec604ea421a63f2788e64b0fcef7054d1404a
zethboy <yoginugraha842@gmail.com>
2025-03-03 21:38:01 +0700

    homepage kelas done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import {Container, Row, Col} from "react-bootstrap";
-import HeroImg from "../assets/img/hero.png"
+import HeroImg from "../assets/img/hero.png";
+import { kelasTerbaru} from "../data/index"
+
+import {Navigate, useNavigate} from "react-router-dom"
 
 const HomePage = () => {
+  let navigate = useNavigate();
   return (
     <div className="homepage">
       <header className="w-100 min-vh-100 d-flex align-items-center">
         <Container>
-          <Row className="header-box d-flex align-items-center">
+          <Row className="header-box d-flex align-items-center pt-lg-5">
             <Col lg="6">
               <h1 className="mb-4">Temukan <br /> <span>Bakat Kreatifmu</span><br /> Bersama Kami!</h1>
               <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt commodi velit id cupiditate, vitae repellat!</p>      
@@ -19,7 +23,56 @@ const HomePage = () => {
           </Row>
         </Container>
       </header>
-      <div className="kelas w-100 min-vh-100"></div>
+      <div className="kelas w-100 min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center fw-bold">
+                Kelas Terbaru
+              </h1>
+              <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </Col>
+          </Row>
+          <Row>
+            {kelasTerbaru.map((kelas)=>{
+              return(
+                <Col key={kelas.id}>
+                  <img src={kelas.image} alt="unsplash.com" className="mb-5 rounded-top"/>
+                  <div className="star mb-2 px-3">
+                    <i className={kelas.star1}></i>
+                    <i className={kelas.star2}></i>
+                    <i className={kelas.star3}></i>
+                    <i className={kelas.star4}></i>
+                    <i className={kelas.star5}></i>
+                  </div>
+                  <h5 className="mb-5 px-3">
+                    {kelas.title}
+                  </h5>
+                  <div className="ket d-flex justify-content-between align-items-center px-3 pb-3">
+                    <p className=" m-0 text-primary fw-bold">{kelas.price}</p>
+                    <button className="btn btn-danger rounded-1">{kelas.buy}</button>
+                  </div>
+                </Col>
+              )
+            })}          
+          </Row>
+          <Row>
+            <Col className="text-center">
+              <button className="btn btn-success rounded-5 btn-lg" onClick={() => navigate("/kelas")}>Lihat Semua Kelas <i className="fa-solid fa-chevron-right ms-1"></i></button>
+             
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="testimonial py-5">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center fw-bold my-5">Testimonial</h1>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   )
 }
