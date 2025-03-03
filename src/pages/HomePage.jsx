9b8763578d58a38744d79301d7986b594af927e8
zethboy <yoginugraha842@gmail.com>
2025-03-03 22:49:48 +0700

    testimonial section  in homepage done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,17 @@
 import {Container, Row, Col} from "react-bootstrap";
 import HeroImg from "../assets/img/hero.png";
-import { kelasTerbaru} from "../data/index"
+import { kelasTerbaru, dataSwiper} from "../data/index"
 
 import {Navigate, useNavigate} from "react-router-dom"
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 const HomePage = () => {
   let navigate = useNavigate();
@@ -36,7 +45,7 @@ const HomePage = () => {
           <Row>
             {kelasTerbaru.map((kelas)=>{
               return(
-                <Col key={kelas.id}>
+                <Col key={kelas.id} className="shadow rounded">
                   <img src={kelas.image} alt="unsplash.com" className="mb-5 rounded-top"/>
                   <div className="star mb-2 px-3">
                     <i className={kelas.star1}></i>
@@ -70,6 +79,49 @@ const HomePage = () => {
             <Col>
               <h1 className="text-center fw-bold my-5">Testimonial</h1>
             </Col>
+          </Row>
+          <Row>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={10}
+            pagination={{
+              clickable: true,
+            }}
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+              },
+              992: {
+                slidesPerView: 2,
+                spaceBetween: 50,
+              },
+              1200: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+              },
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            {dataSwiper.map((data)=> {
+              return <SwiperSlide key={data.id} className="shadow -sm">
+                <p className="desc">{data.desc}</p>
+                <div className="people">
+                  <img src={data.image} alt="" />
+                  <div>
+                    <h5 className="mb-1">{data.name}</h5>
+                    <p className="m-0 fw-bold">{data.skill}</p>
+                  </div>
+                </div>
+              </SwiperSlide>
+            })}
+            
+          </Swiper>
           </Row>
         </Container>
       </div>
