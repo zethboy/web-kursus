134fc1161b5d439814d256637cc4b641019f8388
zethboy <yoginugraha842@gmail.com>
2025-03-03 20:36:17 +0700

    homepage header

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,26 +1,43 @@
+import {useState, useEffect} from "react"
 import { Container, Nav, Navbar } from "react-bootstrap";
 import {navLinks} from "../data/index"
 import {NavLink} from "react-router-dom"
 
 const NavbarComponent = () => {
+  const [changeColor, setChangeColor] =useState(false);
+  const changeBackgroundColor = () => {
+    if (window.scrollY > 10){
+      setChangeColor(true);
+    }
+    else{
+      setChangeColor(false);
+    }
+  }
+
+  useEffect (() => {
+    changeBackgroundColor();
+    window.addEventListener("scroll", changeBackgroundColor)
+  })
+
   return <div>
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={changeColor ? "color-active" : ""} >
       <Container>
-        <Navbar.Brand href="home">Giogi.</Navbar.Brand>
+        <Navbar.Brand href="#" className="fs-3 fw-bold">Giogi.</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
           {navLinks.map((link)=>{
               return(
-              <div className="nav-link" key={link.id}>
-                <NavLink to={link.path}>{link.text}</NavLink>  
+              <div className="nav-link text-center" key={link.id}>
+                <NavLink to={link.path} className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "active" : ""} end>{link.text}</NavLink>  
               </div>
                );
           })
           }
           </Nav>
-          <div>
-            <button>Join With Us</button>
+          <div className="text-center">
+            <button className="btn btn-outline-danger rounded-1">Join With Us</button>
           </div>
         </Navbar.Collapse>
       </Container>
